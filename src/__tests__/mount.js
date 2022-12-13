f2b467aafbf2ad646ff7829e39fe56572203bcdc
Fabs <fabiscg79@gmail.com>
2022-12-13 12:31:42 -0500

    nothing to commit

--- src/__tests__/mount.js
+++ src/__tests__/mount.js
@@ -1,8 +1,9 @@
+// @ts-nocheck
 import { render, fireEvent } from '@testing-library/vue';
 import Home from "../views/Home.vue";
 
 test('Render Home', async () => {
     // The render method returns a collection of utilities to query your component.
     const { getByText } = render(Home);
-
+    getByText("Login to yuor account")
 })
